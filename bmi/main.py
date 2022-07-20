df06ee72ad4997ae8992eedba0bbd30bb620eaef
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2022-07-20 18:42:17 +0200

    first commit

---
+++ bmi/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Small Python Projects for Beginners
+Version: 1.0
+Python 3.10+
+Date created: July 16th, 2022
+Date modified: -
+"""
+
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
