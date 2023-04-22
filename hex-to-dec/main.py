8756e64ae90f62c637e8faa7a23bd8af3b63ae76
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2023-04-22 17:09:17 +0200

    add new example

---
+++ hex-to-dec/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+"""
+Small Python Projects for Beginners
+
+Version: 1.0
+Python 3.11
+Date created: April 22nd, 2023
+Date modified: -
+"""
+
+
+def hex_to_dec():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
