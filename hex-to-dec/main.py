eda31ee0435464ab63b526d6681174f530e7b8d2
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2023-04-23 16:27:22 +0200

    finish hex_digit example

--- hex-to-dec/main.py
+++ hex-to-dec/main.py
@@ -10,13 +10,28 @@ Date created: April 22nd, 2023
 Date modified: -
 """
 
+import sys
 
-def hex_to_dec():
-    pass
+
+def hex_to_dec(hex_digit: str):
+    if hex_digit <= "F" and hex_digit >= "A":
+        # The ord() function returns the ASCII code for
+        # the given character.
+        value = ord(hex_digit) - ord("A") + 10
+        print(f"The decimal value for {hex_digit} is {value}.")
+    elif hex_digit.isdigit():
+        print(f"The decimal value for {hex_digit} is {hex_digit}.")
+    else:
+        print("The input value is invalid!")
 
 
 def main():
-    pass
+    hex_digit = input("Enter a hex digit: ").upper()
+
+    if len(hex_digit) != 1:
+        sys.exit("You entered more than digit!")
+
+    hex_to_dec(hex_digit)
 
 
 if __name__ == "__main__":
