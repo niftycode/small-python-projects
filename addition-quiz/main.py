f831d033c2d3ede4945ebd8271ada05f3a1c4c74
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2023-04-23 18:12:25 +0200

    finish addition quiz example

--- addition-quiz/main.py
+++ addition-quiz/main.py
@@ -3,10 +3,7 @@
 
 """
 Small Python Projects for Beginners
-
-Addition Quiz: The user can enter a new answer until it is correct.
-This program has no exception handling. So, if the user enter a character
-the program will crash.
+Convert a hex digit to decimal
 
 Version: 1.0
 Python 3.11
@@ -14,23 +11,28 @@ Date created: April 22nd, 2023
 Date modified: -
 """
 
-# import sys
-import random
+import sys
+
+
+def hex_to_dec(hex_digit: str):
+    if hex_digit <= "F" and hex_digit >= "A":
+        # The ord() function returns the ASCII code for
+        # the given character.
+        value = ord(hex_digit) - ord("A") + 10
+        print(f"The decimal value for {hex_digit} is {value}.")
+    elif hex_digit.isdigit():
+        print(f"The decimal value for {hex_digit} is {hex_digit}.")
+    else:
+        print("The input value is invalid!")
 
 
 def main():
-    number1 = random.randint(0, 9)
-    number2 = random.randint(0, 9)
+    hex_digit = input("Enter a hex digit: ").upper()
 
-    result = number1 + number2
+    if len(hex_digit) != 1:
+        sys.exit("You entered more than digit!")
 
-    answer = int(input(f"What is {number1} + {number2}? "))
-
-    while result != answer:
-        print("Wrong!")
-        answer = int(input(f"What is {number1} + {number2}? "))
-
-    print(f"Yes, {number1} + {number2} is {result}.")
+    hex_to_dec(hex_digit)
 
 
 if __name__ == "__main__":
