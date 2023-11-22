01eee2c3002615e3645eebf6ba56c564209d5dbe
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2023-11-22 15:25:34 +0100

    add volume example

---
+++ box-volume/main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+"""
+Volume of a Box Calculator
+Formula: length * width * height
+
+This code example shows how to calculate the volume of a box.
+The formula for the calculation is very simple and therefore
+does not matter much. Rather, it comes down to the following:
+
+-   Input of data by the user
+-   Exception handling
+-   Using a Class (object-oriented programming)
+
+
+Version: 1.1
+Python 3.12
+Date created: March 17th, 2023
+Date modified: November 22nd, 2023
+"""
+
+import sys
+
+
+class BoxCalculator:
+    def __init__(self, length: int, width: int, height: int) -> None:
+        self.length = length
+        self.width = width
+        self.height = height
+
+    @property
+    def length(self):
+        return self.__length
+
+    @length.setter
+    def length(self, length):
+        self.__length = length
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}"
+            f"({self.length}, "
+            f"{self.width}, "
+            f"{self.height})"
+        )
+
+    def calculate_volume(self) -> int:
+        return self.length * self.width * self.height
+
+
+def main():
+    length = input("Enter the length: ")
+    width = input("Enter the width: ")
+    height = input("Enter the height: ")
+
+    try:
+        box_calculator = BoxCalculator(int(length), int(width), int(height))
+    except Exception as e:
+        print(f"Wrong type! {e}")
+        sys.exit()
+
+    volume = box_calculator.calculate_volume()
+    print(f"The volume is: {volume}")
+
+
+if __name__ == "__main__":
+    main()
