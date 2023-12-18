f9b8f4c6e2d7aec9a9ee47be8ad7d93f4023252b
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2023-12-18 18:36:41 +0100

    Update BMI Calculator and add test script
    The BMI Calculator (main.py) was updated to include method documentation for calculate_bmi() and main(). The version was also updated from 1.0 to 1.1. Additionally, a test script (test_bmi.py) has been added to test the functionality of the calculate_bmi() method.

--- bmi-1/main.py
+++ bmi-1/main.py
@@ -4,14 +4,28 @@
 """
 Small Python Projects for Beginners
 BMI Calculator
-Version: 1.0
+Version: 1.1
 Python 3.10+
 Date created: July 16th, 2022
-Date modified: July 20th, 2022
+Date modified: December 18th, 2023
 """
 
 
 def calculate_bmi(weight: float, height: float):
+    """
+    Calculates the Body Mass Index (BMI) using the given weight and height.
+
+    Args:
+        weight (float): The weight of the person in kilograms.
+        height (float): The height of the person in meters.
+
+    Returns:
+        None
+
+    Raises:
+        None
+
+    """
     bmi: float = weight / (height * height)
 
     if bmi < 18.5:
@@ -23,6 +37,10 @@ def calculate_bmi(weight: float, height: float):
 
 
 def main():
+    """
+    Entry point of this program.
+    Get weight and height entered by the user and invoke calculate_bmi().
+    """
     weight: float = float(input("Please enter your weight (kg): "))
     height: float = float(input("Please enter your height (m): "))
 
