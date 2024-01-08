f87ca82360bc0e68934b48dd777e666934004218
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2024-01-08 16:04:54 +0100

    add new example

---
+++ calculate/calculate.md
@@ -0,0 +1,4 @@
+# calculate
+
+This example shows how to use operators (+, -, *, /). The user enters two numbers and then decides which mathematical operation is to be performed.
+
---
+++ calculate/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""
+This example shows how to use operators (+, -, *, /).
+Version: 1.0
+Python 3.12+
+Date created: January 8th, 2024
+Date modified: -
+"""
+
+
+def add(number_one, number_two):
+    return number_one + number_two
+
+
+def subtract(number_one, number_two):
+    return number_one - number_two
+
+
+def multiply(number_one, number_two):
+    return number_one * number_two
+
+
+def divide(number_one, number_two):
+    return number_one / number_two
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
