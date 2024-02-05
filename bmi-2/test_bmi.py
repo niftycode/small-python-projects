9ffa79cfc312a9154a69dc9f82f9819a6f5ba94a
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2024-02-05 12:06:13 +0100

    add more tests

--- bmi-2/test_bmi.py
+++ bmi-2/test_bmi.py
@@ -10,22 +10,43 @@ def under_height():
 
 @pytest.fixture
 def under_weight():
-    return 79.4
+    return 59.0
 
 
-def test_calculate_bmi_normal_weight(under_weight, under_height, capsys):
+@pytest.fixture
+def normal_height():
+    return 1.82
+
+
+@pytest.fixture
+def normal_weight():
+    return 79.6
+
+
+@pytest.fixture
+def over_height():
+    return 1.82
+
+
+@pytest.fixture
+def over_weight():
+    return 101.0
+
+
+def test_calculate_bmi_under_weight(under_weight, under_height, capsys):
     calculate_bmi(under_weight, under_height)
-    captured = capsys.readouterr()
-    assert captured.out == "Normal weight\n"
+    captured = capsys.readouterr().out.rstrip()
+    assert captured == "Underweight"
 
 
-@pytest.mark.skip(reason="no test available")
-def test_calculate_bmi_overweight(weight, height):
-    # TODO - 21.01.2024: Add more tests
-    ...
+# @pytest.mark.skip(reason="no test available")
+def test_calculate_bmi_normal_weight(normal_weight, normal_height, capsys):
+    calculate_bmi(normal_weight, normal_height)
+    captured = capsys.readouterr().out.rstrip()
+    assert captured == "Normal weight"
 
 
-@pytest.mark.skip(reason="no test available")
-def test_calculate_bmi_underweight(weight, height):
-    # TODO - 21.01.2024: Add more tests
-    ...
+def test_calculate_bmi_overweight(over_weight, over_height, capsys):
+    calculate_bmi(over_weight, over_height)
+    captured = capsys.readouterr().out.rstrip()
+    assert captured == "Overweight"
--- calculate/calculate.md
+++ calculate/calculate.md
@@ -1,4 +1,3 @@
 # calculate
 
 This example shows how to use operators (+, -, *, /). The user enters two numbers and then decides which mathematical operation is to be performed.
-
