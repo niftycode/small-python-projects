87501978efe3379ce814b46d4cc17e088b12167b
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2024-12-02 16:40:05 +0100

    add tests

--- calculate/test_calculate.py
+++ calculate/test_calculate.py
@@ -1,3 +1,27 @@
 import pytest
 
-# TODO - 22.01.2024: Add tests
+
+@pytest.fixture
+def number_one():
+    return 30
+
+
+@pytest.fixture
+def number_two():
+    return 15
+
+
+def test_add(number_one, number_two):
+    assert 45
+
+
+def test_subtract(number_one, number_two):
+    assert 15
+
+
+def test_multiply(number_one, number_two):
+    assert 450
+
+
+def test_divide(number_one, number_two):
+    assert 2
